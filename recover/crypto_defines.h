7eddeb3dd042f266c5edc3afc8d3ced81782b353
Santiago Torres <torresariass@gmail.com>
2014-12-01 19:56:03 -0500

    Adds base specification to the recover routine.

--- recover/crypto_defines.h
+++ recover/crypto_defines.h
@@ -4,9 +4,41 @@
 #include <openssl\sha.h>
 #include <openssl\evp.h>
 #include <openssl\hmac.h>
+#include <sys/stat.h>
 
-#define DEFAULT_DIGEST "sha256"
+/* this defines hold the selected algorithms to use */
+#define ENC_ALGO EVP_aes_256_cbc()
+#define ENC_BLOCKSIZE EVP_CIPHER_block_size(ENC_ALGO)
+#define DIGEST EVP_sha384()
+#define BLOCKSIZE EVP_MD_block_size(DIGEST)
 
-// function declarations
-unsigned char* generate_file_key(const unsigned char *filename, unsigned char* key, 
+/* Function declarations */
+
+/* Dec receives two file pointers (source and destination) and a key filename
+ *	as a string. The contents of inFilePoiinter are encrypted using the key in filename and
+ *  the result is written to outFilePointer.
+ */
+void dec(FILE *inFilePointer, FILE *outFilePointer, unsigned char *key);
+
+/* generate_file_key produces a filename from an hmac key (oftentimes a string), 
+ *   the length of the resulting key (in bytes) is written to file_key_length
+ */
+unsigned char* generate_file_key(const unsigned char *filename, unsigned char* key,
 	unsigned int keylength, unsigned int *file_key_length);
+
+/*  binary_to_string produces a hex-encoded printable string from an input
+ *  binary stream (data), the result is written to char* filename. Filename must be 2xlength+1 
+ *  long in order to hold the result of the data input. 
+ */
+void binary_to_string(char *filename, unsigned char *data, unsigned int
+length);
+
+/*  string_to_binary produces a binary representation of an hex-encoded string.
+ *  In other words, is the inverse operation of the previous function. 
+ */
+void string_to_binary(unsigned char *binary, char *string, unsigned int length);
+
+/* load_key loads a key from an ascii-encoded file and returns it's binary 
+ * equivalent.
+ */
+unsigned char *load_key(char *filename);
--- recover/main.c
+++ recover/main.c
@@ -1,55 +1,170 @@
 #include "crypto_defines.h"
+#include "c99_code.h"
 
 
 
-main(int argc, char *argv[])
- {
+void dec(FILE *inFilePointer, FILE *outFilePointer, unsigned char* key){
 
- unsigned char *filename, *key;
- unsigned int i, key_length;
- OpenSSL_add_all_digests();
-
- filename = generate_file_key((unsigned char *)"file.txt", (unsigned char *)"Secret", 
-	 strlen("Secret"), &key_length);
-
- puts("Encrypted Filename:" );
- for (i = 0; i < key_length; i++)
-	 printf("%02x", filename[i]);
-
- printf("\n");
-
- key = generate_file_key(filename, (unsigned char *)"Secret", strlen("Secret"), &key_length);
-
- puts("Encryption key:");
- for ( i = 0; i < key_length; i++)
-	 printf("%02x", key[i]);
-
- printf("\n");
-
- free(key);
- getchar();
- exit(0);
-
- }
+	int inFileSize;
+	EVP_CIPHER_CTX ctx; /*Decryption Object/context*/
+	int outputLength1 = 0;
+	int outputLength2 = 0;
+	unsigned char *inputData;
+	unsigned char *outputData;
+	unsigned int length;
+	unsigned int blocksize;
+	unsigned char *initVector;
+	int i, keyFileSize;
 
 
-unsigned char* generate_file_key(const unsigned char *filename, unsigned char* key, 
-	unsigned int keylength, unsigned int *file_key_length)
+
+	/*Get Size of Input File*/
+	fseek(inFilePointer, 0L, SEEK_END);
+	inFileSize = ftell(inFilePointer);
+	/*Put it back to the Beginning*/
+	fseek(inFilePointer, 0L, SEEK_SET);
+
+	/* blocksize is handy all around :) */
+	blocksize = ENC_BLOCKSIZE;
+	
+	/* append a buffer of blocksize as per the spec */
+	inputData = (unsigned char *)malloc(sizeof(*inputData) * (inFileSize + blocksize));
+	outputData = (unsigned char *)malloc(((sizeof(*inputData) * inFileSize) + sizeof(length) / blocksize + 1 * blocksize));
+
+	/* populate the IV and write it to the file */
+	printf("\t[Decrypt] Reading IV...");
+	initVector = (unsigned char *)malloc(sizeof(*initVector) * blocksize);
+	fread(initVector, sizeof(unsigned char), blocksize, inFilePointer);
+	printf("done!\n");
+
+	/*Read the input File*/
+	printf("\t[Decrypt] Reading encrypted data...");
+	fread(inputData, sizeof(char), inFileSize - blocksize, inFilePointer);
+	printf("done!\n");
+
+	/*Setup and Execute decryption*/
+	printf("\t[Decrypt] Decrypting...");
+	EVP_DecryptInit(&ctx, ENC_ALGO, key, initVector);   /*Init the Context */
+	EVP_DecryptUpdate(&ctx, outputData, &outputLength1, inputData, inFileSize); /*Decrypt */
+	EVP_DecryptFinal(&ctx, outputData + outputLength1, &outputLength2);  /*Final Output and length */
+	printf("done!\n");
+
+	/* obtain the original file's length */
+	printf("\t[Decrypt] Obtaining file length...");
+	memcpy(&length, outputData, sizeof(length));
+	printf("Obtained length %u...", outputLength1);
+	printf("done!\n");
+
+	/* write the file... */
+	printf("\t[Decrypt] Writing out the file...");
+	fwrite(outputData + sizeof(length), sizeof(char), length, outFilePointer); /*write the truncated output data to file*/
+	printf("done!\n");
+
+	/*Cleanup! Necessary since were not using _ex version of above functions*/
+	EVP_CIPHER_CTX_cleanup(&ctx);
+
+	free(initVector);
+	free(inputData);
+	free(outputData);
+	return;
+}
+
+void binary_to_string(char *filename, unsigned char *data, unsigned int
+length)
 {
 
+    unsigned int i;
+	
+    for (i = 0; i < length; i++) {
+        snprintf(filename + 2*i, 3, "%02x", data[i]);
+    }
+    filename[2*length + 1] = '\0';
+
+}
+
+void string_to_binary(unsigned char *binary, char *string, unsigned int length)
+{
+
+    unsigned int i;
+    char bytevalue[3];
+    bytevalue[2] = '\0';
+
+    for (i = 0; i < length; i+=2) {
+        memcpy(bytevalue, string + i, 2);
+        sscanf(bytevalue, "%02hhx", binary + (i/2));
+    }
+
+	return;
+}
+
+unsigned char* generate_file_key(const unsigned char *filename, unsigned char* key,
+	unsigned int keylength, unsigned int *file_key_length)
+{
 	unsigned char* file_key = (unsigned char *)malloc(sizeof(*file_key) * EVP_MAX_MD_SIZE);
 	unsigned int len;
-	const EVP_MD *md;
 	HMAC_CTX ctx;
 
-	md = EVP_get_digestbyname(DEFAULT_DIGEST);
-	
-
-
 	HMAC_CTX_init(&ctx);
-	HMAC_Init_ex(&ctx, key, keylength, md, NULL);
+	HMAC_Init_ex(&ctx, key, keylength, DIGEST, NULL);
 	HMAC_Update(&ctx, filename, strlen((char *)filename));
-	HMAC_Final(&ctx, file_key, file_key_length); 
+	HMAC_Final(&ctx, file_key, file_key_length);
 
 	return file_key;
+}
+
+/* load_key loads a key from an ascii-encoded file and returns it's binary 
+ * equivalent.
+ */
+unsigned char *load_key(char *filename)
+{
+	FILE *fp;
+	unsigned char *key;
+	char *text;
+
+	fp = fopen(filename, "rt");
+	if (!fp) {
+		return NULL;
+	}
+
+	text = (char *)malloc(sizeof(*text) * (BLOCKSIZE * 2) + 1);
+	fgets(text, BLOCKSIZE, fp);
+
+	key = (unsigned char *)malloc(sizeof(*key) * BLOCKSIZE);
+	string_to_binary(key, text, strlen(text));
+
+	fclose(fp);
+	free(text);
+	return key;
+
+}
+
+int main(int argc, char * argv[]){
+
+
+	FILE *FileIN;
+	FILE *FileOUT;
+	unsigned char *key;
+
+	if (argc != 3){
+		printf("[ERROR] Usage Must be: 'recover.exe Key File'\n");
+		return -1;
+	}
+
+	/*Decrypt the file*/
+	FileIN = fopen(argv[2], "rb");/*Cipher text file to be Decrypted*/
+	FileOUT = fopen("decrypted", "wb");/*Decrypted ciphertext will be written here*/
+	printf("[Key derivation] Loading key....");
+	key = load_key((char *)argv[1]);  /* Key file to be read */
+	printf("done!\n");
+
+	printf("[Recover] Preparing for decryption...\n");
+	dec(FileIN, FileOUT, key);
+	printf("done!\n");
+
+	printf("[Revocer] Success! The file was recovered and placed under the filename \"decrypted\"\n");
+	fclose(FileIN);
+	fclose(FileOUT);
+	free(key);
+
+	return 0;
 }
